39e1b6c947f523ec2bbcccc8e1405374a36b2b72
Nattita <nattita_b@cmu.ac.th>
2017-04-06 16:38:33 +0700

    refactor package name

---
+++ src/main/java/camt/se234/unittest/entity/User.java
@@ -0,0 +1,63 @@
+package camt.se234.unittest.entity;
+
+import lombok.Builder;
+
+import java.time.LocalDate;
+
+@Builder
+public class User {
+    String username;
+    String password;
+    String name;
+    LocalDate dateOfBirth;
+    String telephoneNo;
+
+    public User(String username, String password, String name, LocalDate dateOfBirth, String telephoneNo) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.telephoneNo = telephoneNo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (username != null ? !username.equals(user.username) : user.username != null) return false;
+        if (password != null ? !password.equals(user.password) : user.password != null) return false;
+        if (name != null ? !name.equals(user.name) : user.name != null) return false;
+        if (dateOfBirth != null ? !dateOfBirth.equals(user.dateOfBirth) : user.dateOfBirth != null) return false;
+        return telephoneNo != null ? telephoneNo.equals(user.telephoneNo) : user.telephoneNo == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = username != null ? username.hashCode() : 0;
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (dateOfBirth != null ? dateOfBirth.hashCode() : 0);
+        result = 31 * result + (telephoneNo != null ? telephoneNo.hashCode() : 0);
+        return result;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+}
